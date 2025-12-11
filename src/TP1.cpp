0251936fe567b66bae2840ea344f37083ec5a089
Wey Théo <wey.theo@outlook.fr>
2025-12-11 15:16:04 +0100

    Initial commit

---
+++ src/TP1.cpp
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+#include "stdint.h"
+
+uint8_t digitPins[] = {2, 3, 4, 5};
+uint8_t numSegments = 8;
+uint8_t segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+uint8_t tabDeco7seg[] = {0x3f, 0x06, 0x5b, 0x4F, 0x66, 0x6d, 0x7d, 0x07,
+                         0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71};
+
+void setup(void) {
+}
+
+void loop(void) {
+}
